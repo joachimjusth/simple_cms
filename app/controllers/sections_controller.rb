04b2ffa789b3081cae038d8d17e7e0be6d9319a0
Joachim Justh <me@joachimjusth.com>
2016-08-18 22:17:09 +0200

    Add Scope

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,7 +1,7 @@
 class SectionsController < ApplicationController
 
   def index
-    @sections = Section.all
+    @sections = Section.sorted
   end
 
   def show
--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,9 @@ class Page < ApplicationRecord
   belongs_to :subject
   has_and_belongs_to_many :editors, :class_name => 'AdminUser' #, :join_table => 'custom join table name'
 
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order('pages.position ASC') }
+  scope :newest_first, lambda { order('pages.created DESC') }
+
 end
