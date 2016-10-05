0110b86d1586ea08e5b5c662133374315008bcdc
Joachim Justh <me@joachimjusth.com>
2016-10-05 22:33:26 +0200

    Validates Method

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,16 @@ class Page < ApplicationRecord
 
   # Indicate the foreign key in table
   belongs_to :subject
+  has_many :sections
   has_and_belongs_to_many :editors, :class_name => 'AdminUser' #, :join_table => 'custom join table name'
+
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 255
+  validates_presence_of :permalink
+  validates_length_of :permalink, :within => 3..255
+  # use presence of with length_of to disallow spaces
+  validates_uniqueness_of :permalink
+  # for unique values by subject use ':scope => :subject_id'
 
   scope :visible, lambda { where(:visible => true) }
   scope :invisible, lambda { where(:visible => false) }
