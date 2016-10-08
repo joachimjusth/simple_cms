7fd24a01f90c66d88cbf6c6cf0312eea269359e6
Joachim Justh <me@joachimjusth.com>
2016-10-08 21:26:33 +0200

    Login & Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-
   root :to => 'demo#index'
+
+  get 'admin', :to => 'access#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match ':controller(/:action(/:id(.:format)))', :via => [:get, :post]
 end
