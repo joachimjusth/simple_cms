cab14d69cbea1a63507712d45fb2ab993111ac92
Joachim Justh <me@joachimjusth.com>
2016-10-08 22:50:16 +0200

    Neested form

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,9 +3,10 @@ class SectionsController < ApplicationController
   layout 'admin'
 
   before_action :confirm_logged_in
+  before_action :find_page
 
   def index
-    @sections = Section.sorted
+    @sections = @page.sections.sorted
   end
 
   def show
@@ -13,14 +14,14 @@ class SectionsController < ApplicationController
   end
 
   def new
-    @section = Section.new
+    @section = Section.new(:page_id => @page.id)
   end
 
   def create
     @section = Section.new(section_parameters)
     if @section.save
       flash[:notice] = 'Section created successfully.'
-      redirect_to(:action => 'index')
+      redirect_to(:action => 'index', :page_id => @page.id)
     else
       render('new')
     end
@@ -34,7 +35,7 @@ class SectionsController < ApplicationController
     @section = Section.find_by_id(params[:id])
     if @section.update_attributes(section_parameters)
       flash[:notice] = 'Section updated successfully.'
-      redirect_to(:action => 'show', :id => @section.id)
+      redirect_to(:action => 'show', :id => @section.id, :page_id => @page.id)
     else
       render('edit')
     end
@@ -47,13 +48,17 @@ class SectionsController < ApplicationController
   def destroy
     section = Section.find_by_id(params[:id]).destroy
     flash[:notice] = "Section '#{section.name}' destroyed successfully."
-    redirect_to(:action => 'index')
+    redirect_to(:action => 'index', :page_id => @page.id)
   end
 
   private
 
     def section_parameters
       params.require(:section).permit(:page_id, :name, :content_type, :content, :position, :visible)
+    end
+
+    def find_page
+      @page = Page.find_by_id(params[:page_id])
     end
 
 end
