b67112192532afef44e0d08cd89dde6bcddd7b4a
Joachim Justh <me@joachimjusth.com>
2016-10-09 15:22:38 +0200

    Logging to log files

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -35,4 +35,13 @@ class DemoController < ApplicationController
     #render(:text => "1" + 1) # can't convert type
   end
 
+  def logging
+    logger.debug('This is debug.')
+    logger.info('This is info.')
+    logger.warn('This is warn.')
+    logger.error('This is error.')
+    logger.fatal('This is fatal.')
+    render(:text => 'Logged!')
+  end
+
 end
